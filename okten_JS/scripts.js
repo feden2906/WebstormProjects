bc04655b54acef211f61e8954da18805e738b97f
OlegBilko <bilko.oleg@gmail.com>
2021-07-13 15:03:07 +0300

    create variables in scripts.js

--- okten_JS/scripts.js
+++ okten_JS/scripts.js
@@ -1,1 +1,43 @@
-alert('fnjkd')
+/*create variables*/
+let variable_2 = 'hello';
+let variable_3 = 'owu';
+let variable_4 = 'com';
+let variable_5 = 'ua';
+let variable_6 = 1;
+let variable_7 = 10;
+let variable_8 = -199;
+let variable_9 = 123;
+let variable_10 = 3.14;
+let variable_11 = 2.7;
+let variable_12 = 16;
+let variable_13 = true;
+let variable_14 = false;
+/*to console*/
+console.log(variable_2);
+console.log(variable_3);
+console.log(variable_4);
+console.log(variable_5);
+console.log(variable_6);
+console.log(variable_7);
+console.log(variable_8);
+console.log(variable_9);
+console.log(variable_10);
+console.log(variable_11);
+console.log(variable_12);
+console.log(variable_13);
+console.log(variable_14);
+/*to screen*/
+document.write(variable_2);
+document.write(variable_3);
+document.write(variable_4);
+document.write(variable_5);
+document.write(variable_6);
+document.write(variable_7);
+document.write(variable_8);
+document.write(variable_9);
+document.write(variable_10);
+document.write(variable_11);
+document.write(variable_12);
+document.write(variable_13);
+document.write(variable_14);
+
