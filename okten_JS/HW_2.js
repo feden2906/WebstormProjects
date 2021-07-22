dfe0e29c2c8bc74c327f5eb9a4e713b9852744e5
OlegBilko <bilko.oleg@gmail.com>
2021-07-22 17:59:08 +0300

    end of part 1 of HW JS les_2

--- okten_JS/HW_2.js
+++ okten_JS/HW_2.js
@@ -117,6 +117,7 @@ document.write(`</div>`);*/
  */
 //......................................template3.1...................................................................
 
+/*
 <div>
     <div class="product-card">
         <h3 class="product-title">TITLE. Price - PRICE</h3>
@@ -124,4 +125,37 @@ document.write(`</div>`);*/
         </div>
                 інші об'єкти з масиву
                 ...
-</div>
+</div>*/
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+document.write(`<div class = 'product-card'>`)
+
+for (const product of products) {
+    document.write(`
+   <div class="wrap-item">
+    <h3 class = "product-title"> ${product.title}. <br> Price - ${product.price} UAH </h3> 
+    <img class = 'product-image' src="${product.image}">
+    </div>
+`)
+}
+document.write(`</div>`);
