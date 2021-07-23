460fbfb99a695032ad4fcae395a1addbdee11c06
OlegBilko <bilko.oleg@gmail.com>
2021-07-23 17:38:58 +0300

    classwork_2 complete

--- okten_JS/classwork/classwork_2_JS.js
+++ okten_JS/classwork/classwork_2_JS.js
@@ -87,11 +87,26 @@
 }*/
 //- Створити цикл for на 100 ітерацій з кроком 1.
 // Вивести поточний номер кроку через console.log та document.write
+
 /*for (var i = 1; i <= 100; i++) {
-   console.log(i);}*/
-   //document.write(i);}
+   console.log(i);
+   document.write(i)}*/
+
 //- Створити цикл for на 100 ітерацій з кроком 2.
 // Вивести поточний номер кроку через console.log та document.write
 /*for (var i = 1; i <= 100; i = i + 2) {
     console.log(i);}*/
 //- Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+
+/*
+for (let i = 2; i <= 100; i = i+2) {
+    console.log(i);
+    document.write(i);
+}*/
+
+//- Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+/*
+for (let i = 1; i <= 100; i = i+2) {
+    console.log(i);
+    document.write(i);
+}*/
