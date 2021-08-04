cf8231d16e966af0d5cdf6cd938296314eb62356
OlegBilko <bilko.oleg@gmail.com>
2021-08-04 16:47:21 +0300

    problem url

--- okten_JS/mini_project/mini_project.js
+++ okten_JS/mini_project/mini_project.js
@@ -1,17 +1,23 @@
 
+let usersDiv = document.getElementsByClassName('allUsers')[0];
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(value => value.json())
 
     .then(users => {
-        let usersDiv = document.getElementsByClassName('allUsers')[0];
-        for (let userIdName of users) {
+
+        for (let user of users) {
             let idName = document.createElement('div');
-            idName.innerText = `${userIdName.id} - ${userIdName.name}`;
+            idName.innerText = `${user.id} - ${user.name}`;
             idName.classList.add('each_user');
             usersDiv.append(idName);
 
             let btn = document.createElement('button');
             btn.innerText = 'user details';
             usersDiv.append(btn);
+
+            btn.href = `user-details.html?user=${JSON.stringify(user)}`
+            //idName.append(btn);
+            //usersDiv.append(idName);
         }
     });
