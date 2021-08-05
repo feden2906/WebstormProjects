c5e58c341c12f6c177d7fc4b0d3a7867151c21b7
OlegBilko <bilko.oleg@gmail.com>
2021-08-06 01:15:40 +0300

    finish mini_project.js

--- okten_JS/mini_project/mini_project.js
+++ okten_JS/mini_project/mini_project.js
@@ -3,30 +3,29 @@ let usersDiv = document.getElementById('allUsers');
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(value => value.json())
-
     .then(value => {
 
         for (const user of value) {
+            let userContainer = document.createElement('div');
+            userContainer.classList.add('user-container');
 
             let idName = document.createElement('div');
             idName.innerText = user.id + '. ' + user.name;
             idName.classList.add('each_user');
 
+
             let btn = document.createElement('button');
+            btn.classList.add('btn');
             btn.innerText = 'user details';
-            //btn.href = `user-details.html?user=${JSON.stringify(user)}`;
+
+            userContainer.append(idName);
+            userContainer.append(btn);
+            usersDiv.append(userContainer);
+
             btn.onclick = function (){
-                location.href = `user-details.html?user=${JSON.stringify(user)}`;
+                location.href = `user-details.html?user=${JSON.stringify(user)}`; //======>>>
             };
 
-
-
-            usersDiv.append(idName);
-            usersDiv.append(btn);
-
-
-
-
-
         }
+
     });
